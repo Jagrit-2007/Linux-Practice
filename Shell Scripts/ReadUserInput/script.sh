394dfca40fee496226fc9d4eb9b8a8beac40e9be
Jagrit-2007 <jagrit.bansal0@gmail.com>
2026-07-01 22:06:01 +0530

    added content to txt from 10 to 16

--- Shell Scripts/ReadUserInput/script.sh
+++ Shell Scripts/ReadUserInput/script.sh
@@ -14,5 +14,20 @@ echo firstname : $name1 , secondname : $name2 , thirdname : $name3
 
 # ******************************************************************
 
-# taking input at the same line
+# taking input at the same line, so use -p
+# we also now want to take password , which should NOT be visible while typing
+# so use -s , and for entering in same line use -p so -sp , MAKE SURE IT'S -sp NOT -ps
 
+read -p "Please enter your username: " user_name
+read -sp "Enter the password: " user_pass
+echo # now the next echo will be in newline
+echo "Hello $user_name!"
+echo "your password is $user_pass"
+
+# ********************************************************************
+
+# -a : it will tell script to read the array
+
+echo "Enter 3 names: " 
+read -a names
+echo "Names : ${name[0]} , ${name[1]} , ${name[2]}" 
